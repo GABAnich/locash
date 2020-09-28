5ddc468cadb55887cc102a2637f5a4c497ce7390
GABAnich <doofixxx@gmail.com>
2020-09-28 15:22:29 +0300

    Don't display transactions without value

--- message-handler/services/format-stats.js
+++ message-handler/services/format-stats.js
@@ -1,6 +1,7 @@
 module.exports = (stats) => {
     if (!stats.length) return "There no transactions in this time range.";
     return stats
+        .filter((transaction) => transaction && transaction.value)
         .map(
             ({ value, description }) =>
                 `<b>${value > 0 ? "+" : ""}${value}</b> <i>${description}</i>\n`
--- message-handler/test/services/format-stats.test.js
+++ message-handler/test/services/format-stats.test.js
@@ -20,5 +20,18 @@ describe("services", () => {
                 "<b>-20</b> <i>coffee</i>\n<b>+24000</b> <i>salary</i>\n<b>+8500</b> <i>freelance</i>\n"
             );
         });
+        it("should ignore transactions without value", () => {
+            const stats = [
+                { value: -20, description: "coffee" },
+                null,
+                { value: false },
+                { value: +24000, description: "salary" },
+                { value: 8500, description: "freelance" }
+            ];
+
+            expect(formatStats(stats)).to.equal(
+                "<b>-20</b> <i>coffee</i>\n<b>+24000</b> <i>salary</i>\n<b>+8500</b> <i>freelance</i>\n"
+            );
+        });
     });
 });
