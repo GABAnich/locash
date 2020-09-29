7ed78f52d896fa15f83042151ad909eeb3e331bb
GABAnich <doofixxx@gmail.com>
2020-09-29 14:11:40 +0300

    Move footer in separate file

--- message-handler/stats/day.js
+++ message-handler/stats/day.js
@@ -1,16 +1,8 @@
 const format = require("./format-stats");
+const footer = require("./footer");
 
 module.exports = (stats) => {
     if (!stats.length) return "There no transactions in this time range.";
-    let income = 0;
-    let spending = 0;
-    let total = 0;
-    stats.forEach(({ value }) => {
-        if (value > 0) income += value;
-        if (value < 0) spending += value;
-        total += value;
-    });
-    income = income > 0 ? `+${income}` : income;
-    total = total > 0 ? `+${total}` : total;
+    const { income, spending, total } = footer(stats);
     return `${format(stats)}\n<b>Income</b>: ${income}\n<b>Spending</b>: ${spending}\n<b>Total</b>: ${total}\n`;   
 }
---
+++ message-handler/stats/footer.js
@@ -0,0 +1,15 @@
+module.exports = (stats) => {
+    let income = 0;
+    let spending = 0;
+    let total = 0;
+
+    stats.forEach(({ value }) => {
+        if (value > 0) income += value;
+        if (value < 0) spending += value;
+        total += value;
+    });
+    income = income > 0 ? `+${income}` : income;
+    total = total > 0 ? `+${total}` : total;
+    
+    return { income, spending, total };
+}
