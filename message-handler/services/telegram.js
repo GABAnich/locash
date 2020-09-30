903f44b61c8e31532d0339fffcc0393269814f2e
GABAnich <doofixxx@gmail.com>
2020-09-30 14:27:58 +0300

    Add services/telegram

---
+++ message-handler/services/telegram.js
@@ -0,0 +1,11 @@
+const axios = require("axios");
+const { TELEGRAM_TOKEN } = require("../credentials.json");
+
+const sendToUser = async (chat_id, text) =>
+    axios.post(`https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage`, {
+        chat_id,
+        text,
+        parse_mode: "HTML"
+    });
+
+module.exports = { sendToUser };
--- message-handler/stats/index.js
+++ message-handler/stats/index.js
@@ -1,16 +1,8 @@
-const axios = require("axios");
 const moment = require("moment");
+const { sendToUser } = require("../services/telegram");
 const formatDay = require("./day");
 const formatDays = require("./days");
 const getStats = require("./get-stats");
-const { TELEGRAM_TOKEN } = require("../credentials.json");
-
-const sendToUser = async (chat_id, text) =>
-    axios.post(`https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage`, {
-        chat_id,
-        text,
-        parse_mode: "HTML"
-    });
 
 module.exports = async ({ chat, text }) => {
     if (text === "/stats_day") {
