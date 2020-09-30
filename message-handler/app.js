ff49df0319dd175fa3c387b3107d51243d89c4e7
GABAnich <doofixxx@gmail.com>
2020-09-30 10:58:31 +0300

    Add /stats_past_seven_day

--- message-handler/app.js
+++ message-handler/app.js
@@ -30,6 +30,7 @@ You can track your expenses and incomes here.
 /stats_day - your daily transactions.
 /stats_week - your weekly transactions.
 /stats_month - your monthly transactions.
+/stats_past_seven_day - your transactions in last 7 days.
 /help - more information.
 `;
 
@@ -103,6 +104,14 @@ exports.lambdaHandler = async (event) => {
                 chat_id: chat.id,
                 startDate: moment().startOf("month").unix(),
                 endDate: moment().endOf("month").unix()
+            });
+            await sendToUser(chat.id, formatDays(stats));
+            return { statusCode: 200 };
+        } else if (text === "/stats_past_seven_day") {
+            const stats = await getStats({
+                chat_id: chat.id,
+                startDate: moment().subtract(7,'d').startOf("day").unix(),
+                endDate: moment().endOf("day").unix()
             });
             await sendToUser(chat.id, formatDays(stats));
             return { statusCode: 200 };
