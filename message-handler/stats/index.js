6196550644fbcbbef863fdbdd065b99ce0c600a2
GABAnich <doofixxx@gmail.com>
2020-09-30 17:53:09 +0300

    Refactor stats

--- message-handler/stats/index.js
+++ message-handler/stats/index.js
@@ -5,39 +5,55 @@ const { commandNotFound } = require("../text");
 const formatDay = require("./day");
 const formatDays = require("./days");
 
+const statsDay = async (chat) => {
+    const stats = await getTransactions({
+        chat_id: chat.id,
+        startDate: moment().startOf("day").unix(),
+        endDate: moment().endOf("day").unix(),
+    });
+    await sendToUser(chat.id, formatDay(stats));
+    return { statusCode: 200 };
+};
+
+const statsWeek = async (chat) => {
+    const stats = await getTransactions({
+        chat_id: chat.id,
+        startDate: moment().startOf("week").unix(),
+        endDate: moment().endOf("week").unix(),
+    });
+    await sendToUser(chat.id, formatDays(stats));
+    return { statusCode: 200 };
+};
+
+const statsMonth = async (chat) => {
+    const stats = await getTransactions({
+        chat_id: chat.id,
+        startDate: moment().startOf("month").unix(),
+        endDate: moment().endOf("month").unix(),
+    });
+    await sendToUser(chat.id, formatDays(stats));
+    return { statusCode: 200 };
+};
+
+const statsPastSevenDay = async (chat) => {
+    const stats = await getTransactions({
+        chat_id: chat.id,
+        startDate: moment().subtract(7, "d").startOf("day").unix(),
+        endDate: moment().endOf("day").unix(),
+    });
+    await sendToUser(chat.id, formatDays(stats));
+    return { statusCode: 200 };
+};
+
 module.exports = async ({ chat, text }) => {
     if (text === "/stats_day") {
-        const stats = await getTransactions({
-            chat_id: chat.id,
-            startDate: moment().startOf("day").unix(),
-            endDate: moment().endOf("day").unix(),
-        });
-        await sendToUser(chat.id, formatDay(stats));
-        return { statusCode: 200 };
+        return statsDay(chat);
     } else if (text === "/stats_week") {
-        const stats = await getTransactions({
-            chat_id: chat.id,
-            startDate: moment().startOf("week").unix(),
-            endDate: moment().endOf("week").unix(),
-        });
-        await sendToUser(chat.id, formatDays(stats));
-        return { statusCode: 200 };
+        return statsWeek(chat);
     } else if (text === "/stats_month") {
-        const stats = await getTransactions({
-            chat_id: chat.id,
-            startDate: moment().startOf("month").unix(),
-            endDate: moment().endOf("month").unix(),
-        });
-        await sendToUser(chat.id, formatDays(stats));
-        return { statusCode: 200 };
+        return statsMonth(chat);
     } else if (text === "/stats_past_seven_day") {
-        const stats = await getTransactions({
-            chat_id: chat.id,
-            startDate: moment().subtract(7, "d").startOf("day").unix(),
-            endDate: moment().endOf("day").unix(),
-        });
-        await sendToUser(chat.id, formatDays(stats));
-        return { statusCode: 200 };
+        return statsPastSevenDay(chat);
     } else {
         await sendToUser(chat.id, commandNotFound);
         return { statusCode: 200 };
