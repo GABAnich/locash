27e608bd21f80ae07f4f741a78772af5b0b8266f
GABAnich <doofixxx@gmail.com>
2020-09-30 14:31:21 +0300

    Remove getStats

--- message-handler/app.js
+++ message-handler/app.js
@@ -33,25 +33,6 @@ const createTransaction = ({ chat_id, date, value, description }) =>
         })
         .promise();
 
-const getStats = async ({ chat_id, startDate, endDate }) =>
-    db
-        .query({
-            TableName,
-            KeyConditionExpression:
-                "#chat_id = :chat_id And #date BETWEEN :start_date AND :end_date",
-            ExpressionAttributeValues: {
-                ":chat_id": chat_id,
-                ":start_date": startDate,
-                ":end_date": endDate
-            },
-            ExpressionAttributeNames: {
-                "#chat_id": "chat_id",
-                "#date": "date"
-            }
-        })
-        .promise()
-        .then(({ Items }) => Items);
-
 exports.lambdaHandler = async (event) => {
     try {
         console.log(event);
