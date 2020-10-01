97c187d4c5054fa18c67614b0fdfda91d0673423
GABAnich <doofixxx@gmail.com>
2020-10-01 14:58:45 +0300

    Add /stats_month_[month]_[year]

--- message-handler/stats/index.js
+++ message-handler/stats/index.js
@@ -4,6 +4,7 @@ const { sendToUser } = require("../services/telegram");
 const { commandNotFound } = require("../text");
 const formatDay = require("./day");
 const formatDays = require("./days");
+const parseStatsSpecificMonth = require("./parse-stats-specific-month");
 
 const statsDay = async (chat) => {
     const stats = await getTransactions({
@@ -35,6 +36,22 @@ const statsMonth = async (chat) => {
     return { statusCode: 200 };
 };
 
+const statsSpecificMonth = async ({ chat, text }) => {
+    const res = parseStatsSpecificMonth(text);
+    if (!res) {
+        await sendToUser(chat.id, commandNotFound);
+        return { statusCode: 200 };
+    }
+    const { month, year } = res;
+    const stats = await getTransactions({
+        chat_id: chat.id,
+        startDate: moment().month(month).year(year).startOf("month").unix(),
+        endDate: moment().month(month).year(year).endOf("month").unix(),
+    });
+    await sendToUser(chat.id, formatDays(stats));
+    return { statusCode: 200 };
+};
+
 const statsPastSevenDay = async (chat) => {
     const stats = await getTransactions({
         chat_id: chat.id,
@@ -52,6 +69,8 @@ module.exports = async ({ chat, text }) => {
         return statsWeek(chat);
     } else if (text === "/stats_month") {
         return statsMonth(chat);
+    } else if (text.startsWith("/stats_month")) {
+        return statsSpecificMonth({ chat, text });
     } else if (text === "/stats_past_seven_day") {
         return statsPastSevenDay(chat);
     } else {
