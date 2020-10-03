1c15f4b7e0cf457d4f34f0ff485f2278f85c785c
GABAnich <doofixxx@gmail.com>
2020-10-03 16:55:51 +0300

    Handle error, when text is undefined

--- message-handler/app.js
+++ message-handler/app.js
@@ -1,10 +1,14 @@
 const { sendToUser } = require("./services/telegram");
 const handleStats = require("./stats");
 const handleTransaction = require("./transaction");
-const { welcomeText } = require("./text");
+const { welcomeText, pleaseTryAgain } = require("./text");
 const { TELEGRAM_TOKEN } = require("./credentials.json");
 
 const handleMessage = async ({ chat, text, date }) => {
+    if (!text) {
+        sendToUser(chat.id, pleaseTryAgain);
+        return { statusCode: 200 };
+    }
     if (text === "/start" || text === "/help") {
         await sendToUser(chat.id, welcomeText);
         return { statusCode: 200 };
