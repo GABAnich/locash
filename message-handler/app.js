85887f48246d4869f5a18c60afab7cc8b1ce7230
GABAnich <doofixxx@gmail.com>
2020-10-13 22:14:48 +0300

    Revert "Use process.env.TELEGRAM_TOKEN"
    This reverts commit 459f56137ac1a9a0a5e4e652b6bb1ab011fb0abc.

--- message-handler/app.js
+++ message-handler/app.js
@@ -2,8 +2,7 @@ const { sendToUser } = require("./services/telegram");
 const handleStats = require("./stats");
 const handleTransaction = require("./transaction");
 const { welcomeText, pleaseTryAgain } = require("./text");
-
-const TELEGRAM_TOKEN = process.env.TELEGRAM_TOKEN;
+const { TELEGRAM_TOKEN } = require("./credentials.json");
 
 const handleMessage = async ({ chat, text, date }) => {
     if (!text) {
