0a7d2604da264faa75b0cef9f19116c0a69567e8
GABAnich <doofixxx@gmail.com>
2020-10-18 16:40:28 +0300

    Fix lang for /lang_[]

--- message-handler/language.js
+++ message-handler/language.js
@@ -1,14 +1,15 @@
 const { sendToUser } = require("./services/telegram");
 const { setLanguage } = require("./services/dynamodb");
+const allLabels = require("./text/text");
 
 module.exports = async ({ chat, text, labels }) => {
     if (text === "/lang_en") {
         await setLanguage({ chat_id: chat.id, language: "en" });
-        await sendToUser(chat.id, labels.done);
+        await sendToUser(chat.id, allLabels.done.en);
         return { statusCode: 200 };
     } else if (text === "/lang_ua") {
         await setLanguage({ chat_id: chat.id, language: "ua" });
-        await sendToUser(chat.id, labels.done);
+        await sendToUser(chat.id, allLabels.done.ua);
         return { statusCode: 200 };
     } else {
         await sendToUser(chat.id, labels.commandNotFound);
