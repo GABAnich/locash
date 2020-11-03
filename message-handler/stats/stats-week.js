aec2ba1294ecc5c2377253627b05cd429fc2df74
GABAnich <doofixxx@gmail.com>
2020-11-03 20:44:42 +0200

    Add chart for stats-week

--- message-handler/stats/stats-week.js
+++ message-handler/stats/stats-week.js
@@ -1,14 +1,23 @@
 const moment = require("moment");
 const { getTransactions } = require("../services/dynamodb");
 const formatDays = require("./days");
-const { sendToUser } = require("../services/telegram");
+const { sendPhoto } = require("../services/telegram");
+const {
+    barCharDays: { getEncodedUrl },
+} = require("../services/quickchart");
 
 module.exports = async (chat, labels) => {
+    const startDate = moment().startOf("week").unix();
+    const endDate = moment().endOf("week").unix();
     const stats = await getTransactions({
         chat_id: chat.id,
-        startDate: moment().startOf("week").unix(),
-        endDate: moment().endOf("week").unix(),
+        startDate,
+        endDate,
     });
-    await sendToUser(chat.id, formatDays(stats, labels));
+    await sendPhoto({
+        chat_id: chat.id,
+        photo: getEncodedUrl({ stats, labels, startDate, endDate }),
+        caption: formatDays(stats, labels),
+    });
     return { statusCode: 200 };
 };
