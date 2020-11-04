5ac7595500ba002da38bfd85d8a2b19300f4679e
GABAnich <doofixxx@gmail.com>
2020-11-04 21:24:48 +0200

    Add chart for stats-specific-month

--- message-handler/stats/stats-specific-month.js
+++ message-handler/stats/stats-specific-month.js
@@ -1,8 +1,17 @@
 const moment = require("moment");
 const { getTransactions } = require("../services/dynamodb");
-const { sendToUser } = require("../services/telegram");
+const { sendToUser, sendPhoto } = require("../services/telegram");
 const formatDays = require("./days");
+const footer = require("./footer");
 const parseStatsSpecificMonth = require("./parse-stats-specific-month");
+const pieChartWithIncomeSpendingTotal = require("../services/quickchart/pieChartWithIncomeSpendingTotal");
+
+const getStats = ({ chat_id, month, year }) =>
+    getTransactions({
+        chat_id,
+        startDate: moment().month(month).year(year).startOf("month").unix(),
+        endDate: moment().month(month).year(year).endOf("month").unix(),
+    });
 
 module.exports = async ({ chat, text, labels }) => {
     const res = parseStatsSpecificMonth(text);
@@ -11,11 +20,16 @@ module.exports = async ({ chat, text, labels }) => {
         return { statusCode: 200 };
     }
     const { month, year } = res;
-    const stats = await getTransactions({
+    const stats = await getStats({ chat_id: chat.id, month, year });
+    const { income, spending, total } = footer(stats);
+    await sendPhoto({
         chat_id: chat.id,
-        startDate: moment().month(month).year(year).startOf("month").unix(),
-        endDate: moment().month(month).year(year).endOf("month").unix(),
+        photo: pieChartWithIncomeSpendingTotal(
+            { income, spending, total },
+            labels
+        ),
+        caption: formatDays(stats, labels),
     });
-    await sendToUser(chat.id, formatDays(stats, labels));
+
     return { statusCode: 200 };
 };
