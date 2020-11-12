64c290f35e8c34c3335b946f66bc8537d7752108
GABAnich <doofixxx@gmail.com>
2020-11-12 23:19:59 +0200

    use stats-accumulator

--- message-handler/stats/stats-all.js
+++ message-handler/stats/stats-all.js
@@ -2,10 +2,10 @@ const formatMonths = require("./months");
 const { getAllTransactions } = require("../services/dynamodb");
 const { sendPhoto } = require("../services/telegram");
 const { getEncodedUrl } = require("../services/quickchart/lineChart");
+const statsAccumulator = require("./stats-accumulator");
 
 module.exports = async (chat, labels) => {
-    const stats = await getAllTransactions(chat.id);
-    // await sendToUser(chat.id, formatMonths(stats, labels));
+    const stats = statsAccumulator(await getAllTransactions(chat.id));
     await sendPhoto({
         chat_id: chat.id,
         photo: getEncodedUrl({ stats, labels }),
