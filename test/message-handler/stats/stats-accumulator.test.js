d2d9114f2f8cedfa88062e143a182d20dc8dc7e4
GABAnich <doofixxx@gmail.com>
2020-11-12 23:13:17 +0200

    Cover rest props

--- test/message-handler/stats/stats-accumulator.test.js
+++ test/message-handler/stats/stats-accumulator.test.js
@@ -1,17 +1,22 @@
 const { expect } = require("chai");
 const statsAccumulator = require("../../../message-handler/stats/stats-accumulator");
 
-const stats = [{ value: 1 }, { value: 2 }, { value: 3 }, { value: 4 }];
+const stats = [
+    { value: 1, rest: "rest" },
+    { value: 2, rest: "rest2" },
+    { value: 3, rest: "rest3" },
+    { value: 4, rest: "rest4" },
+];
 
 describe("message-handler", () => {
     describe("stats", () => {
         describe("stats-accumulator", () => {
             it("should return accumulated stats", () => {
                 expect(statsAccumulator(stats)).to.be.deep.equal([
-                    { value: 1 },
-                    { value: 3 },
-                    { value: 6 },
-                    { value: 10 },
+                    { value: 1, rest: "rest" },
+                    { value: 3, rest: "rest2" },
+                    { value: 6, rest: "rest3" },
+                    { value: 10, rest: "rest4" },
                 ]);
             });
             it("shoult return [] if no stats", () => {
