27ace7aaa83fa890be8b23d668a78a9f69bfe053
GABAnich <doofixxx@gmail.com>
2020-12-03 23:27:57 +0200

    Remove credentials.json

--- message-handler/config.js
+++ message-handler/config.js
@@ -1,9 +1,7 @@
-const { LOCAL_ENDPOINT } = require("./credentials.json");
-
 const config = {};
 
 if (process.env.AWS_SAM_LOCAL) {
-    config.endpoint = LOCAL_ENDPOINT;
+    config.endpoint = "http://172.17.0.2:8000";
 }
 
 module.exports = config;
