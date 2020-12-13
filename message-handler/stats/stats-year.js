8b2fad4d6bde240523faae00f6e9ae4046344b31
GABAnich <doofixxx@gmail.com>
2020-12-13 16:08:16 +0200

    Display chart for /stats_year

--- message-handler/stats/stats-year.js
+++ message-handler/stats/stats-year.js
@@ -1,7 +1,9 @@
 const moment = require("moment");
 const { getTransactions } = require("../services/dynamodb");
-const { sendToUser } = require("../services/telegram");
+const { sendPhoto } = require("../services/telegram");
 const formatMonths = require("./months");
+const { getEncodedUrl } = require("../services/quickchart/lineChart");
+const statsAccumulator = require("./stats-accumulator");
 
 module.exports = async (chat, labels) => {
     const stats = await getTransactions({
@@ -9,6 +11,10 @@ module.exports = async (chat, labels) => {
         startDate: moment().startOf("year").unix(),
         endDate: moment().endOf("year").unix(),
     });
-    await sendToUser(chat.id, formatMonths(stats, labels));
+    await sendPhoto({
+        chat_id: chat.id,
+        photo: getEncodedUrl({ stats: statsAccumulator(stats), labels }),
+        caption: formatMonths(stats, labels),
+    });
     return { statusCode: 200 };
 };
