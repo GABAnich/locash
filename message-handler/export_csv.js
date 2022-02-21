fdf86633a3c6b328a822941acf896d4df0bba905
rostyslav.bornitskyi <doofixxx@gmail.com>
2022-02-21 09:40:53 +0200

    Comment out export csv

--- message-handler/export_csv.js
+++ message-handler/export_csv.js
@@ -1,10 +1,11 @@
-const { parse } = require("json2csv");
-const { getAllTransactions } = require("./services/dynamodb");
-const { sendToUser } = require("./services/telegram");
+// const { parse } = require("json2csv");
+// const { getAllTransactions } = require("./services/dynamodb");
+// const { sendToUser } = require("./services/telegram");
 
-module.exports = async ({ chat }) => {
-    const stats = await getAllTransactions(chat.id);
-    const csv = parse(stats, ["date", "description", "value", "chat_id"]);
-    await sendToUser(chat.id, csv);
+// module.exports = async ({ chat }) => {
+module.exports = async () => {
+    // const stats = await getAllTransactions(chat.id);
+    // const csv = parse(stats, ["date", "description", "value", "chat_id"]);
+    // await sendToUser(chat.id, csv);
     return { statusCode: 200 };
 };
