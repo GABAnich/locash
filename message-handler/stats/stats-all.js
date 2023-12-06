31f29f2e3cf6cf56a53b6fa13c8ccf4e0b6036cf
rostyslav.bornitskyi <doofixxx@gmail.com>
2023-12-06 13:44:04 +0200

    do not send image for stats_all

--- message-handler/stats/stats-all.js
+++ message-handler/stats/stats-all.js
@@ -1,15 +1,9 @@
 const formatMonths = require("./months");
 const { getAllTransactions } = require("../services/dynamodb");
-const { sendPhoto } = require("../services/telegram");
-const { getEncodedUrl } = require("../services/quickchart/lineChart");
-const statsAccumulator = require("./stats-accumulator");
+const { sendToUser } = require("../services/telegram");
 
 module.exports = async (chat, labels) => {
     const stats = await getAllTransactions(chat.id);
-    await sendPhoto({
-        chat_id: chat.id,
-        photo: getEncodedUrl({ stats: statsAccumulator(stats), labels }),
-        caption: formatMonths(stats, labels),
-    });
+    await sendToUser(chat.id, formatMonths(stats, labels));
     return { statusCode: 200 };
 };
